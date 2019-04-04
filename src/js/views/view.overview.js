152846107e87c89ee33c020e5d8284fa57097821
Stéphane Huart <mail@stephanehuart.com>
2019-04-05 00:26:45 +0200

    fix #6 overview was showing wrong values

--- src/js/views/view.overview.js
+++ src/js/views/view.overview.js
@@ -13,56 +13,68 @@ var createOverview = function (targetSelector) {
   }
 
   var render = function () {
-    var headerHtml =`
-    <h2 class="ui center aligned icon header">
-      <i class="circular building outline icon"></i>
-      ${store.reference}, ${store.name}
-    </h2>
-    `
-    var html = `
-    <div class="ui very padded container">
-      <div class="ui placeholder segment">
-        <div class="ui four statistics">
-          <div class="statistic">
-            <div class="value">
-              <i class="bullseye icon"></i>
-              ${store.requirements.items.length}
+    var store = query.currentProject()
+    if (store) {
+
+      var headerHtml =`
+      <h2 class="ui center aligned icon header">
+        <i class="circular building outline icon"></i>
+        ${store.reference}, ${store.name}
+      </h2>
+      `
+      var html = `
+      <div class="ui very padded container">
+        <div class="ui placeholder segment">
+          <div class="ui four statistics">
+            <div class="statistic">
+              <div class="value">
+                <i class="comment icon"></i>
+                ${store.requirements.items.length}
+              </div>
+              <div class="label">
+                Requirements
+              </div>
             </div>
-            <div class="label">
-              Requirements
+            <div class="statistic">
+              <div class="value">
+                <i class="users icon"></i>
+                ${store.stakeholders.items.length}
+              </div>
+              <div class="label">
+                Stakeholders
+              </div>
             </div>
-          </div>
-          <div class="statistic">
-            <div class="value">
-              <i class="users icon"></i>
-              ${store.stakeholders.items.length}
+            <div class="statistic">
+              <div class="value">
+                <i class="sitemap icon"></i> ${(store.currentPbs.items.length - 1)}
+              </div>
+              <div class="label">
+                Sub-Systems
+              </div>
             </div>
-            <div class="label">
-              Stakeholders
-            </div>
-          </div>
-          <div class="statistic">
-            <div class="value">
-              <i class="sitemap icon"></i> ${(store.currentPbs.items.length - 1)}
-            </div>
-            <div class="label">
-              Sub-Systems
-            </div>
-          </div>
-          <div class="statistic">
-            <div class="value">
-              <img src="/images/avatar/small/joe.jpg" class="ui circular inline image">
-              ${(store.currentCDC.items.length)}
-            </div>
-            <div class="label">
-              Specs
+            <div class="statistic">
+              <div class="value">
+                <i class="cogs icon"></i> ${(store.functions.items.length)}
+              </div>
+              <div class="label">
+                functions
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    `
-    container.innerHTML = headerHtml+html;
+      `
+      // <div class="statistic">
+      //   <div class="value">
+      //     <img src="/images/avatar/small/joe.jpg" class="ui circular inline image">
+      //     ${(store.currentCDC.items.length)}
+      //   </div>
+      //   <div class="label">
+      //     Specs
+      //   </div>
+      // </div> TODO readd spec when ready
+      container.innerHTML = headerHtml+html;
+    }
   }
 
   var update = function () {
