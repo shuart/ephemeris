697b03b28f25874b2d906409aa572b2518840782
Stéphane Huart <mail@stephanehuart.com>
2019-05-19 16:35:27 +0200

    add color marking to the current view in the top menu bar. Close #14

--- src/js/views/view.topMenu.js
+++ src/js/views/view.topMenu.js
@@ -20,6 +20,17 @@ var createTopMenu = function (containerSelector) {
     document.addEventListener('storeUpdated', function (e) {
       update()
     }, false)
+    document.addEventListener('pageUpdated', function (e) {
+      update()
+      let currentPage =  app.state.currentView;
+      let selectedButtonColor = "#057f75"
+      //set selectedView active
+      let currentActiveButton = container.querySelector('.top_button_'+currentPage)
+      if (currentActiveButton) {
+        // currentActiveButton.classList.add('active')
+        currentActiveButton.style.backgroundColor=selectedButtonColor
+      }
+    }, false)
 
     document.querySelector("#file-project-input").addEventListener("change", readJSON);
     reader.addEventListener("load", function() {
@@ -34,21 +45,20 @@ var createTopMenu = function (containerSelector) {
   }
 
   var render = function () {
-    console.log(app.state.currentProject);
     if (app.state.currentProject) {
       container.innerHTML=`
-      <a class="button action_toogle_unified"><i class="tasks icon"></i><div class="content">Tasks</div></a>
-      <a class="button action_toogle_notes_manager"><i class="sticky note outline icon"></i><div class="content">Notes</div></a>
-        <a class="button action_toogle_overview">
+      <a class="button top_button_unified action_toogle_unified"><i class="tasks icon"></i><div class="content">Tasks</div></a>
+      <a class="button top_button_notesManager action_toogle_notes_manager"><i class="sticky note outline icon"></i><div class="content">Notes</div></a>
+        <a class="button top_button_overview action_toogle_overview">
           <i class="columns icon"></i>
           <div class="content">overview</div>
         </a>
-        <a class="button action_toogle_stakeholders"><i class="address book icon"></i><div class="content">Stakeholders</div></a>
-        <a class="button action_toogle_diag_relations"><i class="sitemap icon"></i><div class="content">Relations</div></a>
-        <a class="button action_toogle_diag_interfaces"><i class="sync icon"></i><div class="content">Interfaces</div></a>
-        <a class="button action_toogle_requirements_view"><i class="comment icon"></i><div class="content">Requirements</div></a>
-        <a class="button action_toogle_functions_view"><i class="cogs icon"></i><div class="content">Functions</div></a>
-        <a class="button action_toogle_tree_pbs"><i class="dolly icon"></i><div class="content">Products</div></a>
+        <a class="button top_button_stakeholders action_toogle_stakeholders"><i class="address book icon"></i><div class="content">Stakeholders</div></a>
+        <a class="button top_button_relations action_toogle_diag_relations"><i class="sitemap icon"></i><div class="content">Relations</div></a>
+        <a class="button top_button_interfaces action_toogle_diag_interfaces"><i class="sync icon"></i><div class="content">Interfaces</div></a>
+        <a class="button top_button_requirements action_toogle_requirements_view"><i class="comment icon"></i><div class="content">Requirements</div></a>
+        <a class="button top_button_functions action_toogle_functions_view"><i class="cogs icon"></i><div class="content">Functions</div></a>
+        <a class="button top_button_pbs action_toogle_tree_pbs"><i class="dolly icon"></i><div class="content">Products</div></a>
       `
       // Removed:
       // <a class="item action_toogle_planning_view"><i class="calendar alternate outline icon"></i>Planning</a>
@@ -56,8 +66,8 @@ var createTopMenu = function (containerSelector) {
       // <a class="item action_toogle_requirements_view"><i class="calculator icon"></i>Borderau</a>
     }else{
       container.innerHTML=`
-        <a class="button action_toogle_unified"><i class="tasks icon"></i><div class="content">Tasks</div></a>
-        <a class="button action_toogle_notes_manager"><i class="sticky note outline icon"></i><div class="content">Notes</div></a>
+        <a class="button top_button_unified action_toogle_unified"><i class="tasks icon"></i><div class="content">Tasks</div></a>
+        <a class="button top_button_notesManager action_toogle_notes_manager"><i class="sticky note outline icon"></i><div class="content">Notes</div></a>
       `
     }
   }
