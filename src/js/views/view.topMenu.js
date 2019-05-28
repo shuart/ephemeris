4d12550f249b7422122f1147135cbfc2a0befcb2
Stéphane Huart <mail@stephanehuart.com>
2019-05-29 01:06:56 +0200

    change tag view button position in top menu

--- src/js/views/view.topMenu.js
+++ src/js/views/view.topMenu.js
@@ -54,12 +54,12 @@ var createTopMenu = function (containerSelector) {
           <div class="content">overview</div>
         </a>
         <a class="button top_button_stakeholders action_toogle_stakeholders"><i class="address book icon"></i><div class="content">Stakeholders</div></a>
+        <a class="button top_button_pbs action_toogle_tags_view"><i class="tags icon"></i><div class="content">Tags</div></a>
         <a class="button top_button_relations action_toogle_diag_relations"><i class="sitemap icon"></i><div class="content">Relations</div></a>
         <a class="button top_button_interfaces action_toogle_diag_interfaces"><i class="sync icon"></i><div class="content">Interfaces</div></a>
         <a class="button top_button_requirements action_toogle_requirements_view"><i class="comment icon"></i><div class="content">Requirements</div></a>
         <a class="button top_button_functions action_toogle_functions_view"><i class="cogs icon"></i><div class="content">Functions</div></a>
         <a class="button top_button_pbs action_toogle_tree_pbs"><i class="dolly icon"></i><div class="content">Products</div></a>
-        <a class="button top_button_pbs action_toogle_tags_view"><i class="tags icon"></i><div class="content">Tags</div></a>
       `
       // Removed:
       // <a class="item action_toogle_planning_view"><i class="calendar alternate outline icon"></i>Planning</a>
