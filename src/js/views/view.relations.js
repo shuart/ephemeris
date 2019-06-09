342a779bd82b3cf154065cc4de3b31019c70d5f1
Stéphane Huart <mail@stephanehuart.com>
2019-06-09 17:02:30 +0200

    add basic svg export

--- src/js/views/view.relations.js
+++ src/js/views/view.relations.js
@@ -169,6 +169,7 @@ var createRelationsView = function () {
       update()
     }, listContainer)
     bind(".action_relations_toogle_show_graph_menu","click",(e)=>{
+      renderImage()
       var elem = queryDOM('.menuGraph')
       if (elem.classList.contains('hidden')) {
         elem.classList.remove('hidden')
@@ -1097,6 +1098,172 @@ var createRelationsView = function () {
     }else if (type = 'functions') {
       push(act.add('functions',{uuid:uuid, name:initValue}))
     }
+  }
+
+  function renderImage() {
+
+
+    let saveFileOnUserDevice = function(file){ // content: blob, name: string
+        if(navigator.msSaveBlob){ // For ie and Edge
+            return navigator.msSaveBlob(file.content, file.name);
+        }
+        else{
+            let link = document.createElement('a');
+            link.href = window.URL.createObjectURL(file.content);
+            link.download = file.name;
+            document.body.appendChild(link);
+            link.dispatchEvent(new MouseEvent('click', {bubbles: true, cancelable: true, view: window}));
+            link.remove();
+            window.URL.revokeObjectURL(link.href);
+        }
+    }
+
+
+
+    function serializeSVG(svg) {//notgood
+      const xmlns = "http://www.w3.org/2000/xmlns/";
+      const xlinkns = "http://www.w3.org/1999/xlink";
+      const svgns = "http://www.w3.org/2000/svg";
+      svg = svg.cloneNode(true);
+      const fragment = window.location.href + "#";
+      const walker = document.createTreeWalker(svg, NodeFilter.SHOW_ELEMENT, null, false);
+      while (walker.nextNode()) {
+        for (const attr of walker.currentNode.attributes) {
+          if (attr.value.includes(fragment)) {
+            attr.value = attr.value.replace(fragment, "#");
+          }
+        }
+      }
+      svg.setAttributeNS(xmlns, "xmlns", svgns);
+      svg.setAttributeNS(xmlns, "xmlns:xlink", xlinkns);
+      const serializer = new window.XMLSerializer;
+      const string = serializer.serializeToString(svg);
+      return new Blob([string], {type: "image/svg+xml"});
+    }
+    function getSVGString( svgNode ) {
+    	svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
+    	var cssStyleText = getCSSStyles( svgNode );
+    	appendCSS( cssStyleText, svgNode );
+
+    	var serializer = new XMLSerializer();
+    	var svgString = serializer.serializeToString(svgNode);
+    	svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink='); // Fix root xlink without namespace
+    	svgString = svgString.replace(/NS\d+:href/g, 'xlink:href'); // Safari NS namespace fix
+
+    	return svgString;
+
+    	function getCSSStyles( parentElement ) {
+    		var selectorTextArr = [];
+
+    		// Add Parent element Id and Classes to the list
+    		selectorTextArr.push( '#'+parentElement.id );
+    		for (var c = 0; c < parentElement.classList.length; c++)
+    				if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
+    					selectorTextArr.push( '.'+parentElement.classList[c] );
+
+    		// Add Children element Ids and Classes to the list
+    		var nodes = parentElement.getElementsByTagName("*");
+    		for (var i = 0; i < nodes.length; i++) {
+    			var id = nodes[i].id;
+    			if ( !contains('#'+id, selectorTextArr) )
+    				selectorTextArr.push( '#'+id );
+
+    			var classes = nodes[i].classList;
+    			for (var c = 0; c < classes.length; c++)
+    				if ( !contains('.'+classes[c], selectorTextArr) )
+    					selectorTextArr.push( '.'+classes[c] );
+    		}
+
+    		// Extract CSS Rules
+    		var extractedCSSText = "";
+    		for (var i = 0; i < document.styleSheets.length; i++) {
+    			var s = document.styleSheets[i];
+
+    			try {
+    			    if(!s.cssRules) continue;
+    			} catch( e ) {
+    		    		if(e.name !== 'SecurityError') throw e; // for Firefox
+    		    		continue;
+    		    	}
+
+    			var cssRules = s.cssRules;
+    			for (var r = 0; r < cssRules.length; r++) {
+    				if ( contains( cssRules[r].selectorText, selectorTextArr ) )
+    					extractedCSSText += cssRules[r].cssText;
+    			}
+    		}
+
+
+    		return extractedCSSText;
+
+    		function contains(str,arr) {
+    			return arr.indexOf( str ) === -1 ? false : true;
+    		}
+
+    	}
+
+    	function appendCSS( cssText, element ) {
+    		var styleElement = document.createElement("style");
+    		styleElement.setAttribute("type","text/css");
+    		styleElement.innerHTML = cssText;
+    		var refNode = element.hasChildNodes() ? element.children[0] : null;
+    		element.insertBefore( styleElement, refNode );
+    	}
+    }
+    function setInlineStyles(svg) {
+      //svg = svg.cloneNode(true);
+
+      function explicitlySetStyle (element) {
+        var cSSStyleDeclarationComputed = getComputedStyle(element);
+        var i, len, key, value;
+        var svgExcludedValues = ['height', 'width', 'min-height', 'min-width'];
+        var computedStyleStr = "";
+        for (i=0, len=cSSStyleDeclarationComputed.length; i<len; i++) {
+          key=cSSStyleDeclarationComputed[i];
+          if (!((element instanceof SVGElement) && svgExcludedValues.indexOf(key) >= 0)) {
+            value=cSSStyleDeclarationComputed.getPropertyValue(key);
+            computedStyleStr+=key+":"+value+";";
+          }
+        }
+        element.setAttribute('style', computedStyleStr);
+      }
+      function traverse(obj){
+        var tree = [];
+        tree.push(obj);
+        visit(obj);
+        function visit(node) {
+          if (node && node.hasChildNodes()) {
+            var child = node.firstChild;
+            while (child) {
+              if (child.nodeType === 1 && child.nodeName != 'SCRIPT'){
+                tree.push(child);
+                visit(child);
+              }
+              child = child.nextSibling;
+            }
+          }
+        }
+        return tree;
+      }
+      // hardcode computed css styles inside SVG
+      var allElements = traverse(svg);
+      var i = allElements.length;
+      while (i--){
+        explicitlySetStyle(allElements[i]);
+      }
+      return svg
+    }
+
+
+
+    let svg = document.querySelector(".stellae-graph")
+    // let blob = new Blob([getSVGString(svg)], {type: "image/svg+xml"})
+    let inlineSVG = setInlineStyles(svg)
+    console.log(inlineSVG);
+    let file = {content:serializeSVG(inlineSVG), name:"test"}
+
+    saveFileOnUserDevice(file)
+
   }
 
   self.setActive = setActive
