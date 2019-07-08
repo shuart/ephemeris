7980fe7655c54e2039ff16779e87cf33db0b0f42
Stéphane Huart <mail@stephanehuart.com>
2019-07-08 23:57:36 +0200

    update nwjs version

--- pack.js
+++ pack.js
@@ -2,7 +2,7 @@ var NwBuilder = require('nw-builder'),
   output = 'build',
   opts = {
     files: './src/**', // use the glob format
-    version: '0.37.1',
+    version: '0.39.2',
     buildDir: output + '/release/',
     cacheDir: output + '/cache/',
 	flavor:"normal",
