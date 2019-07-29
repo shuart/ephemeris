6a9d3fae7ef3507f9d014236086318ed9eee686f
Stéphane Huart <mail@stephanehuart.com>
2019-07-29 21:05:44 +0200

    change extra label color in graph

--- src/js/vendor/stellae.js
+++ src/js/vendor/stellae.js
@@ -454,10 +454,10 @@ function stellae(_selector, _options) {
                 //   return options.customPathIcons[d.labels[0]]["path"]|| "M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32z"
                 // } )//todo chose beter default
               .attr('fill', function (d) {
-                if (d.customColor) {
-                  return class2darkenCustomColor(d.customColor)
+                if (d.customLabelColor) {
+                  return d.customColor
                 }else {
-                  return options.nodeOutlineFillColor ? class2darkenColor(options.nodeOutlineFillColor) : class2darkenColor(d.labels[0]);
+                  return "#73787f";
                 }
               })
               .attr('stroke', function (d) {
@@ -468,7 +468,7 @@ function stellae(_selector, _options) {
                 // }
                 return  '#ffffff';
               })
-              .attr('stroke-width', 15)
+              .attr('stroke-width', 10)
               .attr("transform", function (d) {
                   return "scale("+0.05+") translate(+200, -50)"
                 })
