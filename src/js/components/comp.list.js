d38c25cc2847e0e4fd1ce965d395e6d1a687b345
Stéphane Huart <mail@stephanehuart.com>
2019-08-02 18:00:25 +0200

    change name letters icons background to gradiants

--- src/js/components/comp.list.js
+++ src/js/components/comp.list.js
@@ -717,7 +717,7 @@ function showListMenu({
 
           let letters = showColoredIcons(item)
           let colStyle = 'style ="flex-grow: 0;flex-basis: 50px;"'
-          let style = 'style="background-color: '+colorFromLetters(letters)+';width: 32px;height: 32px;border-radius: 100%;padding: 5px;font-size: 18px;color: white;"'
+          let style = 'style="background: '+colorFromLetters(letters)+';width: 32px;height: 32px;border-radius: 100%;padding: 5px;font-size: 18px;color: white;"'
           nestedHtml +=`
           <div  ${colStyle} data-id="${item[idProp]}" class="column">
             <div ${style} data-id="${item[idProp]}" class="content">
@@ -1002,7 +1002,7 @@ function showListMenu({
     }
   }
 
-  function colorFromLetters(letters) {
+  function colorFromLetters(letters, uniform) {
     // const alphaVal = (s) => s.toLowerCase().charCodeAt(0) - 97 + 1
     const alphaVal = function (s) {
       if (s) {
@@ -1012,10 +1012,15 @@ function showListMenu({
         return alt.toLowerCase().charCodeAt(0) - 97 + 1
       }
     }
-    // let veryDifferentColors = ["#000000","#00FF00","#0000FF","#FF0000","#01FFFE","#FFA6FE","#FFDB66","#006401","#010067","#95003A","#007DB5","#FF00F6","#FFEEE8","#774D00","#90FB92","#0076FF","#D5FF00","#FF937E","#6A826C","#FF029D","#FE8900","#7A4782","#7E2DD2","#85A900","#FF0056","#A42400","#00AE7E","#683D3B","#BDC6FF","#263400","#BDD393","#00B917","#9E008E","#001544","#C28C9F","#FF74A3","#01D0FF","#004754","#E56FFE","#788231","#0E4CA1","#91D0CB","#BE9970","#968AE8","#BB8800","#43002C","#DEFF74","#00FFC6","#FFE502","#620E00","#008F9C","#98FF52","#7544B1","#B500FF","#00FF78","#FF6E41","#005F39","#6B6882","#5FAD4E","#A75740","#A5FFD2","#FFB167","#009BFF","#E85EBE"];
-    let colorNbr = Math.round(( alphaVal(letters[0])+alphaVal(letters[1]) )/78*360)
-    // let color = veryDifferentColors[colorNbr]
-    let color = "hsl("+colorNbr+", 34%, 50%)"
+    let color='#ffffff'
+    if (uniform) {
+      let colorNbr = Math.round(( alphaVal(letters[0])+alphaVal(letters[1]) )/78*360)
+      color = "hsl("+colorNbr+", 34%, 50%)"
+    }else {
+      let colorNbrA = Math.round( alphaVal(letters[0]) /26*360)
+      let colorNbrB = Math.round( alphaVal(letters[1]) /26*360)
+      color = "linear-gradient(127deg, hsl("+colorNbrA+", 34%, 50%), hsl("+colorNbrB+", 34%, 50%))"
+    }
     return color
   }
 
