1f4c897023e104178e389a2194f2f20b8b0775c9
stephane Huart <mail@stephanehuart.com>
2021-12-27 03:50:34 +0100

    update startup view style

--- src/js/startup.js
+++ src/js/startup.js
@@ -1,136 +1,167 @@
 function createStartUp() {
   var self = {}
   var sourceEl;
+  var view = createAdler()
+  var localState={}
+  localState.userData = [{"name":"tesddddt", uuid:"1esefsfes"},{"name":"teaaast", uuid:"2esefsfes"}, {"name":"3tggest", uuid:"esefsfes"},{"name":"teebbbest", uuid:"4esefsfes"}]
 
   function init() {
 
-    render()
-    connect()
+    // render()
+    // connect()
+    setupView()
+    getUsersData()
   }
 
-  function createPBS(projectIn, name) {
-    var projectIn = projectIn || "REF-001, "
-    var projectName = name || "New Project"
-    var store = app.store.projects[0]//TODO remove
-    store.name = projectName
-    app.store.projects[0].name = projectName
-    store.reference = projectIn
-    store.currentPbs.items.push({name: projectIn+projectName, uuid: "ita2215151-a50f-4dd3-904e-146118d5d444"})
-    store.currentPbs.items.push({name: "Sub Category A", uuid:"it23bb697b-9418-4671-bf4b-5410af03dfc3"})
-    store.currentPbs.items.push({name: "Sub Category B", uuid:"it9ba7cc64-970a-4846-b9af-560d8125623e"})
-    store.currentPbs.links.push({source: "ita2215151-a50f-4dd3-904e-146118d5d444", target:"it23bb697b-9418-4671-bf4b-5410af03dfc3"})
-    store.currentPbs.links.push({source: "ita2215151-a50f-4dd3-904e-146118d5d444", target:"it9ba7cc64-970a-4846-b9af-560d8125623e"})
-  }
-
-  function connect() {
-    let file, url, reader = new FileReader;
-
-    sourceEl.onclick = async function(event) {
-        if (event.target.classList.contains("action_startup_submit_item")) {
-          console.log(event.target);
-          console.log(document.querySelector('.input-su-name').value);
-          createPBS(document.querySelector('.input-su-in').value, document.querySelector('.input-su-name').value)
-          //renderCDC(store.db, "")
-          pageManager.setActivePage("projectSelection")
-          renderCDC() //TODO change update mecanism
-          sourceEl.remove()
+  function setupView(){
+    view.addCSS(`
+        .welcomeArea{
+        z-index: 999999999;
+        width:100%;
+        height:100%;
+        background-color:white;
+        position: fixed;
+        top: 0px;
         }
-        if (event.target.classList.contains("action_startup_reload_item")) {//TODO remove when session is removed
-          console.log(event.target);
-          localforage.getItem('sessionProjects').then(function(value) {
-              app.store.projects = value;
-              pageManager.setActivePage("projectSelection")
-              renderCDC() //TODO change update mecanism
-              sourceEl.remove()
-          }).catch(function(err) {
-              // This code runs if there were any errors
-              console.log(err);
-          });
+        .bgBubble{
+          background-color :  white;
+          position: fixed;
+          bottom: 0px;
+          width: 100%;
+          height: 35%;
+          background-image: url('././img/bubble.jpg');
+          background-size:  contain; 
+          background-repeat: no-repeat;
+          background-position: center center;
+          
         }
-        if (event.target.classList.contains("action_startup_load_user")) {
-          dbConnector.getUser(event.target.dataset.id).then(async function (user) {
-            // app.store.projects = user.projects; //TODO use actions //DBCHANGE
-            app.store.userData = user.userData; //TODO use actions
-            app.store.relatedProjects = user.relatedProjects; //TODO use actions
-            app.state.currentUser = user.uuid; //TODO use actions
-            //setup profile if Needed
-            if (!app.store.userData.info.userLastName || !app.store.userData.info.userFirstName) {
-
-              var popup= await createPromptPopup({
-                title:"Complete your profile",
-                iconHeader:"user outline",
-                fields:[
-                  { type:"input",id:"firstName" ,label:"First Name", placeholder:"Set your first name" },
-                  { type:"input",id:"lastName" ,label:"Last Name", placeholder:"Set your last name" },
-                  { type:"input",id:"userId" ,label:"This is your user ID. Change it if you have already one", placeholder:app.store.userData.info.userUuid, optional:true, secondary:true }
-                ]
-              })
-
-              if (!popup) {
-                return undefined
-              }
-
-              app.store.userData.info.userFirstName = popup.result.firstName
-              await dbConnector.setUserInfo(event.target.dataset.id, "userFirstName", app.store.userData.info.userFirstName)
-              app.store.userData.info.userLastName = popup.result.lastName
-              await dbConnector.setUserInfo(event.target.dataset.id, "userLastName", app.store.userData.info.userLastName)
-
-            }
-            if (!app.store.userData.info.userUuid) {
-              app.store.userData.info.userUuid = genuuid()
-            }
-            updateFileForRetroCompatibility()
-            pageManager.setActivePage("projectSelection")
-            sourceEl.remove()
-          }).catch(function(err) {
-              console.log(err);
-          });
+        .eph-title{
+          font-family: 'Poiret One',Lato,'Helvetica Neue',Arial,Helvetica,sans-serif;
         }
-        if (event.target.classList.contains("action_startup_load_reveal")) {
-          function readJSON(e) {
-            reader.readAsText(document.querySelector(".statup_input").files[0]);
-          }
-          //document.querySelector(".statup_input_zone").style.visibility = "visible"
-          document.querySelector(".statup_input").addEventListener("change", readJSON);
-          reader.addEventListener("load", function() {
-            console.log(reader.result);
-            // var textContent = JSON.stringify(reader.result, null, 2);
-            // var jsonContent = JSON.parse(reader.result);
-            // console.log(jsonContent);
-            // app.store.projects = jsonContent
-            loadSavedData(reader.result, function() {
-              renderUserSessionView()
+    `)
+    view.createLens("welcomeContainer",(d)=>`
+        <div class="welcomeArea">
+          <div class="bgBubble"></div>
+            <div class="container has-text-centered">
+              <figure class="image is-96x96 is-inline-block">
+                <img src="./img/app.png">
+              </figure>
+              <p class="title eph-title">${d.title}</p>
+              <div class="box is-inline-block">
+
+                <section>
+                    <p class="subtitle">
+                        User list
+                    </p>
+                    <div class="load_area">
+                    
+                    </div>
+                    <div class="block"></div>
+                    <button class="button is-primary is-light action_startup_add_user">Create a new user</button>
+                    <div class="block"></div>
+                    <div class="columns is-multiline is-desktop ">
+                    
+                    </div>
+                </section>
+              </div>
+            </div>
+        </div>`
+    )
+    view.createLens("card",(p)=>`
+
+        <div class="buttons has-addons is-fullwidth">
+          <button style="min-width: 200px;" class="button is-primary action_load">${p.name}</button>
+          <button class="button is-info is-light action_startup_remove_user">X</button>
+        </div>
+      
+    `)
+
+    let supane = view.addLens("welcomeContainer",{
+      data:{title:"Ephemeris"},
+      on:[
+          [".action_startup_add_user", "click", async ()=>{
+            var popup= await createPromptPopup({
+              title:"Add a new session",
+              imageHeader:"./img/tele.png",
+              fields:{ type:"input",id:"sessionName" ,label:"Session name", placeholder:"Set a name for this new session" }
             })
-            // pageManager.setActivePage("unified")
-            // renderCDC() //TODO change update mecanism
-            // sourceEl.remove()
-          });
-
-          document.querySelector(".statup_input").click();
-        }
-
-        if (event.target.classList.contains("action_startup_add_user")) {
-          var popup= await createPromptPopup({
-            title:"Add a new session",
-            imageHeader:"./img/tele.png",
-            fields:{ type:"input",id:"sessionName" ,label:"Session name", placeholder:"Set a name for this new session" }
-          })
-          // var userName = prompt("Add a user")
-          userName = popup.result
-          if (userName && userName != "") {
-            dbConnector.setUser({name:userName,projects:[]}).then(function () {
-              renderUserSessionView()
-            })
-          }
-        }//end event
-        if (event.target.classList.contains("action_startup_remove_user")) {
-            if (confirm("This will remove the user and all it's projects")) {
-              dbConnector.removeUser(event.target.dataset.id).then(function () {
-                renderUserSessionView()
+            // var userName = prompt("Add a user")
+            userName = popup.result
+            if (userName && userName != "") {
+              dbConnector.setUser({name:userName,projects:[]}).then(function () {
+                getUsersData()
               })
             }
-        }//end event
+            
+          }],
+      ],
+      
+     })
+
+     let userCards = supane.addLens("card",{
+        // data:{name:key}, 
+        data:{nam:"kedzqdzqdzqdqdzqdqzy"}, 
+        for:function(){
+          return localState.userData
+        },
+        on:[
+            [".action_load", "click", (e, p)=>{
+              loadUser(p.uuid)
+          } ],
+            [".action_startup_remove_user", "click", (e,p)=>{
+                if (confirm("This will remove "+ p.name +" and all it's projects")) {
+                  dbConnector.removeUser(p.uuid).then(function () {
+                    getUsersData()
+                  })
+                }
+            } ]
+          ],
+        }, ".load_area")
+     view.render()
+     if (document.querySelector('.app-loader-cache')) {
+      document.querySelector('.app-loader-cache').remove();
     }
+
+  }
+
+  function loadUser(uuid){
+    dbConnector.getUser(uuid).then(async function (user) {
+      // app.store.projects = user.projects; //TODO use actions //DBCHANGE
+      app.store.userData = user.userData; //TODO use actions
+      app.store.relatedProjects = user.relatedProjects; //TODO use actions
+      app.state.currentUser = user.uuid; //TODO use actions
+      //setup profile if Needed
+      if (!app.store.userData.info.userLastName || !app.store.userData.info.userFirstName) {
+
+        var popup= await createPromptPopup({
+          title:"Complete your profile",
+          iconHeader:"user outline",
+          fields:[
+            { type:"input",id:"firstName" ,label:"First Name", placeholder:"Set your first name" },
+            { type:"input",id:"lastName" ,label:"Last Name", placeholder:"Set your last name" },
+            { type:"input",id:"userId" ,label:"This is your user ID. Change it if you have already one", placeholder:app.store.userData.info.userUuid, optional:true, secondary:true }
+          ]
+        })
+
+        if (!popup) {
+          return undefined
+        }
+
+        app.store.userData.info.userFirstName = popup.result.firstName
+        await dbConnector.setUserInfo(uuid, "userFirstName", app.store.userData.info.userFirstName)
+        app.store.userData.info.userLastName = popup.result.lastName
+        await dbConnector.setUserInfo(uuid, "userLastName", app.store.userData.info.userLastName)
+
+      }
+      if (!app.store.userData.info.userUuid) {
+        app.store.userData.info.userUuid = genuuid()
+      }
+      updateFileForRetroCompatibility()
+      pageManager.setActivePage("projectSelection")
+      view.remove()
+    }).catch(function(err) {
+        console.log(err);
+    });
   }
 
   function render() {
@@ -140,7 +171,7 @@ function createStartUp() {
   }
   function update() {
     app.state.currentUser=undefined //TODO move to actions
-    sourceEl.remove()
+    // sourceEl.remove()
     init()
   }
   function showLoader() {
@@ -149,127 +180,22 @@ function createStartUp() {
     renderHTML()
   }
 
-  function renderUserSessionView() {
-    var targetDOM = queryDOM('.startup_userlist')//get hook with html generated by startup
-    var html =""
+  function getUsersData(){
+    let userArray =[]
     dbConnector.getUsers().then(function (users) {
       console.log(users);
-      html+= `
-      <div class="ui center aligned container">
-      <div class="ui horizontal divider">Log in</div>`
-      if (users[0]) {
-        html +=`<div class="ui list">`
-        users.forEach(function (user) {
-          html+= `
-            <div class="item">
-            <div class="ui labeled button" tabindex="0">
-              <div data-id="${user.uuid}" style="min-width: 140px;" class="ui teal button action_startup_load_user">
-                <i class="user icon"></i> ${user.name}
-              </div>
-              <a data-id="${user.uuid}" class="ui basic label action_startup_remove_user">
-                X
-              </a>
-            </div>
-            </div>
-            `
+      users.forEach(function (user) {
+        userArray.push({
+          uuid:user.uuid,
+          name:user.name,
         })
-        html+= `
-        </div>
-        <button class="ui center aligned basic button action_startup_add_user">Create a new user</button>
-        </div>`
-      }else {
-        html+= `
-          <button class="ui center aligned big teal button action_startup_add_user">Create a new user</button>
-        </div>`
-      }
-      targetDOM.innerHTML = html //inject new HTML
+      })
+      localState.userData = userArray
+      view.update()
     }).catch(function(err) {
     // This code runs if there were any errors
         console.log(err);
     });
-  }
-
-  function renderHTML() {
-
-    sourceEl = document.createElement('div');
-    sourceEl.style.display= "flex";
-    sourceEl.style.justifyContent= "center";
-    document.querySelector('body').appendChild(sourceEl);
-    var dimmer = document.createElement('div');
-    dimmer.classList="dimmer"
-    // dimmer.style.background = "linear-gradient(#b0d6be, #535f5e)"
-    // dimmer.style.background = "linear-gradient(#bad4c4, #535f5e)"
-
-    // dimmer.style.background = "linear-gradient(#99b6c1, #4a524b)"//grad only
-    dimmer.style.backgroundColor =  "white"
-    // dimmer.style.backgroundImage =  "url('././img/bubble.jpg')"
-    dimmer.style.backgroundRepeat= "no-repeat";
-    dimmer.style.backgroundPosition =  "center bottom"
-    // dimmer.style.backgroundPosition =  "25% 0%"
-    // dimmer.style.background = "linear-gradient(to bottom, #bbd2c5, #536976)"
-    dimmer.style.opacity = 1;
-    var mainEl = document.createElement('div');
-
-    // mainEl.classList ="ui raised very padded text container segment"
-    mainEl.style.maxWidth = "600px"
-    mainEl.style.height = "60%"
-    mainEl.style.overflow = "auto"
-    mainEl.style.position = "fixed"
-    mainEl.style.top = "30px"
-    // mainEl.style.right = "14%"
-    // mainEl.style.width = "50%"
-    mainEl.style.zIndex = "99999"
-    // mainEl.style.backgroundColor = "white"
-    // mainEl.style.top= "50%";
-    // mainEl.style.transform= "translate(-50%, -50%);"
-
-    bgEl = document.createElement('div');
-
-    bgEl.style.backgroundColor =  "white"
-    bgEl.style.position= "fixed"
-    bgEl.style.bottom= "0px"
-    bgEl.style.width= "50%"
-    bgEl.style.height= "35%"
-    bgEl.style.position= "fixed"
-    bgEl.style.backgroundImage= "url('././img/bubble.jpg')"
-    bgEl.style.backgroundSize=     "contain";                      /* <------ */
-    bgEl.style.backgroundRepeat= "no-repeat";
-    bgEl.style.backgroundPosition= "center center";
-    bgEl.style.zIndex = "999999"
-
-    sourceEl.appendChild(dimmer)
-    sourceEl.appendChild(mainEl)
-    sourceEl.appendChild(bgEl)
-    mainEl.innerHTML = `
-    <div class="ui container">
-      <h2 class='ui center aligned icon header'>
-        <img class="ui image" src="./img/app.png">
-        <!-- <i class="circular compass outline icon"></i> -->
-        <div class="ui content app-title">
-        Ephemeris
-        </div>
-      </h2>
-      <div class="startup_userlist">
-        <div class="ui horizontal divider">Log in</div>
-        <div class="ui vertical loading segment">
-          <p></p>
-          <p></p>
-        </div>
-      </div>
-
-      <div class="ui horizontal divider">-</div>
-        <!-- <div class="ui center aligned container">
-          <button class="ui button action_startup_load_reveal" type="submit">load a database file</button>
-          <div style="visibility:hidden" class="statup_input_zone">
-            <input class="ui input statup_input" type="file" accept=".json" />
-          </div>
-        </div> -->
-      </div>
-    </div>
-    `
-    if (document.querySelector('.app-loader-cache')) {
-      document.querySelector('.app-loader-cache').remove();
-    }
   }
 
   function updateFileForRetroCompatibility() {
@@ -296,4 +222,4 @@ function createStartUp() {
 }
 
 var startupScreen = createStartUp()
-startupScreen.init()
+// startupScreen.init()
---
+++ src/js/vendor/adler.js
@@ -0,0 +1,265 @@
+function createComponent(name, string){
+    var self = {}
+    var textString = stringv
+    var name = name
+
+    function render(params) {
+        return textString
+    }
+    self.render = render
+    return self
+}
+
+
+function createAdler({
+    container = document.body,
+    parentLens = undefined,
+    type = "root",
+    parent= undefined,
+    lenses = {},
+    name='root',
+    textString ="",
+    params={},
+    renderAt=undefined,
+    wrapperClass="",
+  } = {}){
+    var self = {}
+    console.log(lenses)
+    // var lenses = Object.assign({},parentLenses )
+    var renderArray = []
+    var instances=[]
+    var wrapper = createWrapper()
+  
+    
+    function addCSS(cssText){
+        var style = document.createElement('style');
+        style.innerHTML = cssText;
+        document.head.appendChild(style);
+    }
+
+    function createLens(name, string, wrapperClass) {
+        // lens[name] = createComponent(name, string)
+        // let newAdler = createAdler({name:name, textString:string, lenses:lenses})
+        //  lenses[name] =newAdler
+        // return newAdler
+
+        let newAdler = {name:name, textString:string, lenses:lenses, wrapperClass: wrapperClass}
+         lenses[name] =newAdler
+        return newAdler
+
+    }
+    let inject = (str, obj) => str.replace(/\%{(.*?)}/g, (x,g)=> obj[g]);
+
+    function createWrapper() {
+        let wrapper ={}
+        wrapper.DOMElement = document.createElement("div")
+        if (wrapperClass != "") {
+            wrapper.DOMElement.classList += ('adler_panel '+wrapperClass)
+        }else{
+            wrapper.DOMElement.classList ="adler_panel"
+        }
+        return wrapper
+
+    }
+
+    function lens(lensName) {
+        return lenses[lensName]
+    }
+    function addLens(lensName,params, renderAt) {
+        let newElement = createAdler({name:lenses[lensName].name, textString:lenses[lensName].textString, lenses:lenses[lensName].lenses,parent:self, container:wrapper, params:params, renderAt:renderAt, wrapperClass:lenses[lensName].wrapperClass})
+        var toRender = {
+            lens:newElement,
+            params:params,
+            renderAt:renderAt,
+        }
+        renderArray.push(toRender)
+        return newElement
+    }
+
+    function render() {
+        if (params.for) {
+            let duplicatedParams = getFormatedParamsCopy(params)
+            wrapper = renderMultiple(duplicatedParams)
+            setUpChildren()
+            mountMultiple()
+        }else{
+            wrapper = renderTemplate(params)
+            setUpChildren()
+            mount()
+        }
+        
+    }
+    function renderMultiple(params) {
+        if (typeof params.for === 'object' && !Array.isArray(params.for) && params.for !== null){
+            // for (const key in params.for) { //NOT IMPLEMENTED YET
+            //     if (user.hasOwnProperty(key)) {
+            
+            //         console.log(`${key}: ${user[key]}`);
+            //     }
+            // }
+        }else if(Array.isArray(params.for)) {
+            let arrayWrapper = createWrapper()
+            for (let index = 0; index < params.for.length; index++) {
+                const element = params.for[index];
+                
+                arrayWrapper.DOMElement.appendChild( renderTemplate(params,index).DOMElement )
+            }
+
+            // arrayWrapper.DOMElement = arrayWrapper.DOMElement.firstElementChild; 
+            return arrayWrapper
+        }else if(true){
+
+        }
+        
+        
+    }
+    function renderTemplate(params,paramIndex) {
+        
+        let localWrapper = createWrapper()
+        
+        var textToDisplay =textString
+        console.log(typeof textString, params.data )
+        if(typeof textString === 'function'){
+            textToDisplay = textString( getParamsData(params,paramIndex) )
+        }else if (params.data) {
+            textToDisplay = inject(textString, getParamsData(params,paramIndex) )
+        }
+        localWrapper.DOMElement.innerHTML = textToDisplay
+        if (localWrapper.DOMElement.childElementCount==1) {
+            localWrapper.DOMElement = localWrapper.DOMElement.firstElementChild; 
+            console.log(localWrapper)
+        }
+        // if (name != "root") {
+        //     if (renderAt) {
+        //         container.querySelector(renderAt).appendChild(wrapper)
+        //     }else{
+        //         container.appendChild(wrapper)
+        //     }
+            
+        // }
+        console.log(localWrapper.DOMElement.innerHTML)
+        
+        if (params && params.on) {
+            console.log(params.on)
+            if ( Array.isArray(params.on[0]) ) {
+                for (let index = 0; index < params.on.length; index++) {
+                    const action = params.on[index];
+                    let target = localWrapper.DOMElement.querySelector(action[0])
+                    if (target) {
+                        function callback(event) {
+                            action[2](event, getParamsData(params,paramIndex))
+                        }
+                        target.addEventListener(action[1],callback)
+                    }
+                }
+            }else{
+                const action = params.on;
+                    let target = localWrapper.DOMElement.querySelector(action[0])
+                    if (target) {
+                        // var callback = function (wrapper) {
+                            
+                        // }
+                        function callback(event) {
+                            action[2](event, getParamsData(params,paramIndex))
+                        }
+                        target.addEventListener(action[1],callback)
+                    }
+            }
+            
+        }   
+        
+        return localWrapper
+    }
+
+    function getParamsData(params,paramIndex) {
+        let newData = params.data || {}
+        if (params.for && (paramIndex || paramIndex==0 )) {
+            newData = Object.assign({},newData, params.for[paramIndex] )
+        }
+        return newData
+        
+    }
+    function getFormatedParamsCopy(params) {
+        let newParams= Object.assign({}, params)
+        if (typeof params.for === 'function') {
+            newParams.for = newParams.for()
+        }
+        return newParams
+        
+    }
+
+
+    function setUpChildren() {
+        for (let index = 0; index < renderArray.length; index++) {
+            const element = renderArray[index];
+            element.lens.render()
+            console.log(params)
+            
+        }
+    }
+    function mount() {
+        if (!parent) {
+            container.appendChild(wrapper.DOMElement)
+        }else if (renderAt) {
+            parent.getElement(renderAt).appendChild(wrapper.DOMElement)
+        }else{
+            parent.getElement('root').appendChild(wrapper.DOMElement)
+        }
+    }
+    function mountMultiple() {
+
+        function move(oldParent, newParent) {
+            while (oldParent.childNodes.length > 0) {
+                newParent.appendChild(oldParent.childNodes[0]);
+                console.debug(newParent)
+            }
+        }
+        if (!parent) {
+            move(wrapper.DOMElement, container)
+        }else if (renderAt) {
+            move(wrapper.DOMElement, parent.getElement(renderAt))
+        }else{
+            move(wrapper.DOMElement, parent.getElement('root'))
+        }
+    }
+
+    function remove() {
+        wrapper.DOMElement.remove()
+    }
+
+    function update() {
+        remove();
+        render();
+    }
+
+    function set(param, value) {
+        if (params) {
+            params[param] = value
+        }
+        render()
+    }
+    function getElement(selector) {
+        if (selector =="root") {
+            return wrapper.DOMElement
+        }else{
+            return wrapper.DOMElement.querySelector(selector)
+        }
+        
+    }
+
+    function setData(newData) {
+        params.data = newData
+    }
+
+    self.render = render
+    self.set = set
+    self.getElement = getElement
+    self.remove = remove
+    self.update = update
+    self.setData = setData
+
+    self.addCSS = addCSS
+    self.addLens = addLens
+    self.createLens = createLens
+    return self
+}
